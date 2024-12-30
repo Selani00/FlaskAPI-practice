c456ffc2076c5678576f29de710a2c59fa30410e
Selani00 <pkselani00@gmail.com>
2024-12-30 10:22:37 +0530

    add query for CRUD oparetions

--- resources/item.py
+++ resources/item.py
@@ -13,28 +13,30 @@ blp = Blueprint("items", __name__, description="Operations on items")
 class Store(MethodView):
     @blp.response(200, ItemSchema)
     def get(self, item_id):
-        try:
-            return items[item_id]
-        except KeyError:
-            abort(404, message="Store not found.")
+        item = ItemModel.query.get_or_404(item_id)
+        return item
 
     def delete(self, item_id):
-        try:
-            del items[item_id]
-            return {"message": "Store deleted."}
-        except KeyError:
-            abort(404, message="Store not found.")
+        item = ItemModel.query.get_or_404(item_id)
+        db.session.delete(item)
+        db.session.commit()
+        return {"message": "Item deleted successfully."}
 
     @blp.arguments(ItemUpdateSchema) # Here we decorate the method with the schema
     @blp.response(200, ItemSchema) # this is down 
-    def put(self,item_data, item_id):
+    def put(self,item_data, item_id):        
+        item = ItemModel.query.get(item_id)
+        if item:
+            item.price = item_data["price"]
+            item.name = item_data["name"]
+        else:
+            item = ItemModel(id=item_id,**item_data)
+
+        db.session.add(item)
+        db.session.commit()
+
+        return item
         
-        try:
-            item = items[item_id]
-            item |= item_data # how to update a dictionary in python
-            return item
-        except KeyError:
-            abort(404, message="Item not found.")
 
 
 @blp.route('/item')
@@ -58,4 +60,4 @@ class Store(MethodView):
     
     @blp.response(200, ItemSchema(many=True))
     def get(self):
-        return items.values()
+        return ItemModel.query.all()
--- resources/store.py
+++ resources/store.py
@@ -2,7 +2,11 @@ import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import abort, Blueprint
-from db import stores
+from db import db
+from models import StoreModel
+from sqlalchemy.exc import SQLAlchemyError,IntegrityError
+
+
 
 from schemas import StoreSchema
 
@@ -13,33 +17,39 @@ blp = Blueprint("stores", __name__, description="Operations on stores")
 class Store(MethodView):
     @blp.response(200, StoreSchema)
     def get(self, store_id):
-        try:
-            return stores[store_id]
-        except KeyError:
-            abort(404, message="Store not found.")
+        store = StoreModel.query.get_or_404(store_id)
+        return store
 
     def delete(self, store_id):
-        try:
-            del stores[store_id]
-            return {"message": "Store deleted."}
-        except KeyError:
-            abort(404, message="Store not found.")
+        store = StoreModel.query.get_or_404(store_id)
+        db.session.delete(store)
+        db.session.commit()
+        return {"message": "Store deleted successfully."}
 
 
 @blp.route('/store')
 class Store(MethodView):
     @blp.response(200, StoreSchema(many=True))
     def get(self):
-        return stores.values()
+        return StoreModel.query.all()
     
     @blp.arguments(StoreSchema) # Here we decorate the method with the schema
     @blp.response(201, StoreSchema) # this is down
     def post(self,store_data):  
 
-        for store in stores.values():
-            if store["name"] == store_data["name"]:
-                abort(400, message="A store with the same name already exists.")
-        store_id  = uuid.uuid4().hex
-        store = {**store_data, "id": store_id}
-        stores[store_id] = store
-        return store
+        store = StoreModel(**store_data) # how to create a dictionary in python
+
+        try:
+            db.session.add(store)
+            db.session.commit()
+
+        except IntegrityError:
+            abort(400, message="A store with that name already exists.")
+
+        except SQLAlchemyError as e:
+            db.session.rollback()
+            abort(400, message="An error occurred while adding the item.")
+
+
+        return  store
+    
